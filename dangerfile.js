544831941dd5561f6b54be5de870a6508b031334
marospecho <67166541+malocheck@users.noreply.github.com>
2024-04-08 20:31:18 +0200

    ahoj danger

--- dangerfile.js
+++ dangerfile.js
@@ -3,17 +3,25 @@ import {message, danger, fail} from "danger"
 const modifiedMD = danger.git.modified_files.join("- ")
 message("Changed Files in this PR: \n - " + modifiedMD)
 
+//Check the length of description
 if (danger.github.pr.body.length < 10) {
     fail("This pull request needs a description")
 }
 
-const baseBranch = danger.github.pr.base.ref
-message("BASE" + baseBranch)
-
-const targetBranch = danger.github.pr.head.ref
+//Name of target branch 
+const targetBranch = danger.github.pr.base.ref
 message("TARGET" + targetBranch)
 
-if (!baseBranch.includes("feature/")) {
-    fail("The base branch is not eligible")
+//Name of base branch
+const baseBranch = danger.github.pr.head.ref
+message("BASE" + baseBranch)
+
+//Rules for merging to develop branch
+if (targetBranch === "develop" && !(baseBranch.includes("feature/") || baseBranch.includes("hotfix/") || baseBranch.includes("fix/"))) {
+    fail("The base branch name" + " " + baseBranch + "is not suitable for develop branch")
 }
 
+//Rules for merging to main branch
+if (targetBranch === "main" && !(baseBranch.includes("develop/") || baseBranch.includes("hotfix/"))) {
+    fail("The base branch name" + " " + baseBranch + "is not suitable for main branch")
+}

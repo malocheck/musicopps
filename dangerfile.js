0b4d217a47389dc6681ba144c7e098a08b66622a
marospecho <67166541+malocheck@users.noreply.github.com>
2024-04-08 20:00:59 +0200

    settings updated

--- dangerfile.js
+++ dangerfile.js
@@ -3,15 +3,15 @@ import {message, danger, fail} from "danger"
 const modifiedMD = danger.git.modified_files.join("- ")
 message("Changed Files in this PR: \n - " + modifiedMD)
 
-if (danger.github.pr.body.length < 10) {
-    fail("This pull request needs a description")
-}
-
 const baseBranch = danger.github.pr.base.ref
 message("BASE" + baseBranch)
 
 const targetBranch = danger.github.pr.head.ref
 message("TARGET" + targetBranch)
+
+if (danger.github.pr.body.length < 10) {
+    fail("This pull request needs a description")
+}
 
 if (!baseBranch.includes("feature/")) {
     fail("The base branch is not eligible")

31f8b761ec61fc71a4affd9bd839d74da6e4bff5
marospecho <67166541+malocheck@users.noreply.github.com>
2024-04-09 13:39:33 +0200

    new function

--- dangerfile.js
+++ dangerfile.js
@@ -8,6 +8,13 @@ message("TARGET" + targetBranch)
 const baseBranch = danger.github.pr.head.ref
 message("BASE" + baseBranch)
 
+//Allowed branch name
+const allowedBranchName = /^(feature|hotfix|fix)\/.*/
+
+if(allowedBranchName.test(danger.git.branch)) {
+    fail("Branch name doesn't match the required format. It should start with 'feature/', 'hotfix/', or 'fix/'.")
+}
+
 //Rules for merging to develop branch
 if (targetBranch === "develop" && !(baseBranch.includes("feature/") || baseBranch.includes("hotfix/") || baseBranch.includes("fix/"))) {
     fail("The base branch name" + " " + baseBranch + "is not suitable for develop branch")
@@ -17,3 +24,5 @@ if (targetBranch === "develop" && !(baseBranch.includes("feature/") || baseBranc
 if (targetBranch === "main" && !(baseBranch.includes("develop/") || baseBranch.includes("hotfix/"))) {
     fail("The base branch name" + " " + baseBranch + "is not suitable for main branch")
 }
+
+

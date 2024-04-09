12b20a234826ba25905559d0b3717f7be0ff9f58
marospecho <67166541+malocheck@users.noreply.github.com>
2024-04-09 12:25:32 +0200

    testing danger

--- dangerfile.js
+++ dangerfile.js
@@ -1,13 +1,5 @@
 import {message, danger, fail} from "danger"
 
-const modifiedMD = danger.git.modified_files.join("- ")
-message("Changed Files in this PR: \n - " + modifiedMD)
-
-//Check the length of description
-if (danger.github.pr.body.length < 10) {
-    fail("This pull request needs a description")
-}
-
 //Name of target branch 
 const targetBranch = danger.github.pr.base.ref
 message("TARGET" + targetBranch)

348bff97d218c1f48b28bbfbc5b499d6a64b0858
marospecho <67166541+malocheck@users.noreply.github.com>
2024-04-09 15:47:42 +0200

    some edits

--- dangerfile.js
+++ dangerfile.js
@@ -1,23 +1,11 @@
-import {message, danger, fail} from "danger"
+import {danger, fail} from "danger"
 
 //Name of target branch 
 const targetBranch = danger.github.pr.base.ref
-message("TARGET" + targetBranch)
-
-//test branch
-const testBranch = danger.git.head
-message("CAUKO" + " " + testBranch)
 
 //Name of base branch
 const baseBranch = danger.github.pr.head.ref
-message("BASE" + baseBranch)
 
-//Allowed branch name
-const allowedBranchName = /^(feature|hotfix|fix)\/.*/
-
-if(allowedBranchName.test(danger.git.branch)) {
-    fail("Branch name doesn't match the required format. It should start with 'feature/', 'hotfix/', or 'fix/'.")
-}
 
 //Rules for merging to develop branch
 if (targetBranch === "develop" && !(baseBranch.includes("feature/") || baseBranch.includes("hotfix/") || baseBranch.includes("fix/"))) {
@@ -25,7 +13,7 @@ if (targetBranch === "develop" && !(baseBranch.includes("feature/") || baseBranc
 }
 
 //Rules for merging to main branch
-if (targetBranch === "main" && !(baseBranch.includes("develop/") || baseBranch.includes("hotfix/"))) {
+if (targetBranch === "main" && !(baseBranch.includes("develop") || baseBranch.includes("hotfix/"))) {
     fail("The base branch name" + " " + baseBranch + "is not suitable for main branch")
 }
 
